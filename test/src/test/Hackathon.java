904932e8d5414f0dc9a070caf148399bda0cb476
Omkar Narveer <omkarnarveer07@gmail.com>
2023-09-15 09:31:55 +0530

    testing commit

---
+++ test/src/test/Hackathon.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Hackathon {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	System.out.println("1. Copy this URL");
+	System.out.println("2. https://github.com/PushpakChaudhari/hackathon.git");
+	
+	}
+}
